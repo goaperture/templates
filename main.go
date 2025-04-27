30f15ab79ab3ed3d94e13be6bfbc895ec29c96b6
II <no_soap_0@mail.ru>
2025-04-27 07:30:25 +0300

    rmx

--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"app/api"
-	"app/api/config"
 	"log"
 )
 
 func main() {
-	client := config.Client{
-		Id:   1,
-		Name: "123",
-	}
-
-	log.Println(client)
-
 	log.Println("Server run on http://localhost:8080")
 	api.Serve(8080, nil)
 }
